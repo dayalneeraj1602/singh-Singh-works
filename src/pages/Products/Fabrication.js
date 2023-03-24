5da304dbcacf0a48caa9642de17d1ddcfb1c67c1
NEERAJ DAYAL <neeraj.dayal.1602@gmail.com>
2023-03-24 10:42:28 +0530

    ll

--- src/pages/Products/Fabrication.js
+++ src/pages/Products/Fabrication.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Services.css";
 import {motion} from 'framer-motion'
-
 const Fabrication = () => {
   const dataInfo = [
     {
       id: 1,
       imgUrl:
-        "https://media.gettyimages.com/id/651480125/photo/man-melting-iron-at-factory.jpg?s=612x612&w=0&k=20&c=n4UnY4xoQY68fN_y4Kvd98VLOmhAmFtWNRJ1mbyREh8=",
+      "https://media.gettyimages.com/id/651480125/photo/man-melting-iron-at-factory.jpg?s=612x612&w=0&k=20&c=n4UnY4xoQY68fN_y4Kvd98VLOmhAmFtWNRJ1mbyREh8=",
       productTitle: "Hello",
       ProductDescription:
         "High-temperature heating device used in industrial processes for material processing.",
