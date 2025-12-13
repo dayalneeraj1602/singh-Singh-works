f294c11173e9096ee618f4ea18a25f09a79ae01f
Neeraj Dayal <neeraj.dayal.1602@gmai.com>
2025-12-13 19:27:45 +0530

    Address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css";
 
 const Footer = () => {
   const companyAddress =
-    "B-74, B Block, Sector 64, Noida Sector 64, Noida - 201301";
+    "B-16, B Block, Sector 64, Noida - 201301";
   const googleMapsLink = `https://www.google.com/maps/dir//${encodeURIComponent(
     companyAddress
   )}/`;
@@ -24,10 +24,17 @@ const Footer = () => {
                 </p>
                 <p>
                   <i className="fa-solid fa-envelope me-2"></i>{" "}
-                  prabhudayal.001@gmail.com
+                  prabhudayal.singh@gmail.com
                 </p>
                 <p>
-                  <i className="fa-solid fa-phone me-2"></i> +91 6280421789
+                  <i className="fa-solid fa-envelope me-2"></i>{" "}
+                  singhengineeringworks.1999@gmail.com
+                </p>
+                <p>
+                  <i className="fa-solid fa-phone me-2"></i> +91 9810802982
+                </p>
+                <p>
+                  <i className="fa-solid fa-phone me-2"></i> +91 9810804532
                 </p>
               </div>
             </div>
@@ -43,8 +50,7 @@ const Footer = () => {
                 </p>
                 <p>
                   {" "}
-                  <i className="fa-solid fa-star me-2"></i>Sun: 9:00 am - 5:00
-                  pm
+                  <i className="fa-solid fa-star me-2"></i>Sun: Holiday
                 </p>
               </div>
             </div>
@@ -52,7 +58,7 @@ const Footer = () => {
 
           <div className="col-md-5 d-flex maps  flex-column">
             {/* Google Maps Integration - Get Directions */}
-            <iframe
+          {/* <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.728712977207!2d77.38255797554164!3d28.60791427567829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce5d3c20bedb7%3A0xc6237134267e953b!2sSingh%20Engineering%20Works!5e0!3m2!1sen!2sin!4v1706278332444!5m2!1sen!2sin"
               width="100%"
               height="300"
@@ -60,7 +66,14 @@ const Footer = () => {
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+            ></iframe> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.5941036364447!2d77.3773047!3d28.6119512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce5d3c20bedb7%3A0xc6237134267e953b!2sSingh%20Engineering%20Works!5e0!3m2!1sen!2sin!4v1765634128605!5m2!1sen!2sin"
+             width="100%"
+              height="300"
+              style={{ borderRadius: " 8px" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"></iframe>
             <div className="mt-4 text-start">
               <a
                 href={googleMapsLink}
