65e4f5dd354b58762be07f5e9d3b8471442239ae
Naveen Basyal <naveenbasyal.001@gmail.com>
2025-12-13 19:39:19 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "../styles/Home.css";
 import { DataContext } from "../Context/DataContext";
 import OurProducts from "./OurProducts";
